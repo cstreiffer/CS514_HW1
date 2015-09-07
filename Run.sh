9646c6f71384bc1b981f976cb71aa1614ff5a255
Kevin Button <klb52@duke.edu>
2015-09-07 18:55:49 -0400

    Added functionality to Run.sh

---
+++ Run.sh
@@ -0,0 +1,7 @@
+udp :: MyUDPChecker{ input->CheckIPHeader() -> IPClassifier(udp,-) -> CheckUDPHeader -> [0]output}
+dpimb :: DPIMB {input -> output}
+
+
+FromDump(MyUDPChecker, ENCAP IP);
+udp[0] -> dpimb;
+ToDump(DPIMB, ENCAP IP);
