a6ca340d24412c5682e9e297ab6e65897f6000bc
Kevin Button <klb52@duke.edu>
2015-09-07 19:14:25 -0400

    Moar run functionality

--- Run.sh
+++ Run.sh
@@ -1,7 +1,9 @@
+#include "~/click/elements"
+
 elementclass MyUDPChecker{ input->CheckIPHeader() -> IPClassifier(udp,-) -> CheckUDPHeader -> [0]output}
 dpimb :: DPIMB {input -> output}
 
 
-FromDump(MyUDPChecker, ENCAP IP);
+FromDevice(MyUDPChecker, ENCAP IP);
 udp[0] -> dpimb;
-ToDump(DPIMB, ENCAP IP);
+ToDevice(DPIMB, ENCAP IP);
