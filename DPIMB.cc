fdff1037f9654e16b562fc8a597cb575d4510164
Christopher Streiffer <cds33@duke.edu>
2015-09-08 00:11:19 -0400

    :

--- DPIMB.cc
+++ DPIMB.cc
@@ -5,7 +5,6 @@ CLICK_DECLS
 
 DPIMB::DPIMB()
 {
-  click_chatter("constructor\n");
   packet_counter = 0;
 
   myInput.open("/home/click/blacklist.txt");
@@ -14,60 +13,48 @@ DPIMB::DPIMB()
 
 int DPIMB::initialize(ErrorHandler *errh) 
 {
-  click_chatter("initialize\n");
   // Load the input file
   std::string line;
-  if(!myOutput.is_open()) {
-      click_chatter("Output not open\n");
-  }
   if(myInput.is_open()) {
     while(getline(myInput, line)) {
       blacklist.push_back(line);
-      click_chatter("Making things\n");
+      click_chatter(line);
     }
     myInput.close();
   }
-  click_chatter("end of initialize-2\n");
 
-  // Create Vector/List of Strings
   return 0;
 } 
 
 bool
 DPIMB::check_blacklist(std::string url)
 {
-  return 1;
-
   // Iterate through list, check against String url
-  // for (std::list<int>::const_iterator iterator = blacklist.begin(), end = blacklist.end(); iterator != end; ++iterator) {
-  //  String black = (String) *iterator;
-  //  if(black.compare(url)==0){
-        // return 1;
-    //}
-  //  std::cout << *iterator;}return 0;
-// 
+  for (Vector<std::string>::const_iterator iterator = blacklist.begin(), end = blacklist.end(); iterator != end; ++iterator) {
+    string black = *iterator;
+    if(black.compare(url)==0){
+        return 1;
+    }
+  }
 
-
+  return 0;
 }
 
 void
 DPIMB::push(int port, Packet *p)
 {
   // Extract DNS url from packet and sent to method check_blacklist
-  std::string url = "Hello";
+  std::string url = "Round 2";
 
-  if(check_blacklist(url)) {
-    myOutput << "Hello" << "\n";
+  if(!check_blacklist(url)) {
+    myOutput << url << "\n";
     p->kill();
 
   } else {    
-    click_chatter("Pushing stuff\n");
     // forward the packet through the output port
     output(0).push(p);
   }
 }
-
-// 
 
 CLICK_ENDDECLS
 EXPORT_ELEMENT(DPIMB)
