d7fb5681928737caa8363548b649630566a979f8
Christopher Streiffer <cds33@duke.edu>
2015-09-08 01:36:39 -0400

    h

--- DPIMB.cc
+++ DPIMB.cc
@@ -51,7 +51,7 @@ DPIMB::push(int port, Packet *p)
   //   std::cout << (std::string) i << '\n';
   // }
 
-  int * udp_port = (int *) p->udp_header();
+  const click_udp * udp_port = p->udp_header();
   std::cout << *udp_port << '\n';
 
 
