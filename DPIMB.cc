793d74294cbefadef37864b5c5ba557abd50dea6
Christopher Streiffer <cds33@duke.edu>
2015-09-08 03:48:18 -0400

    L

--- DPIMB.cc
+++ DPIMB.cc
@@ -50,9 +50,9 @@ DPIMB::push(int port, Packet *p)
   const click_udp *udph = p->udp_header();
   const click_ip *iph = p->ip_header();
 
-  uint16_t source_ip = ntohs(iph->ip_src);
-  uint16_t dest_port = ntohs(udph->uh_dport);
-  uint16_t source_port = ntohs(udph->uh_sport);
+  uint16_t source_ip = (uint16_t) ntohs(iph->ip_src);
+  uint16_t dest_port = (uint16_t) ntohs(udph->uh_dport);
+  uint16_t source_port = (uint16_t) ntohs(udph->uh_sport);
 
   if(dest_port == 53) {
     if(check_blacklist(url)) {
