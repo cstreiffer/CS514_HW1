ee0df8fde72f6c564d2d13d6a43e4e92a730bb16
Christopher Streiffer <cds33@duke.edu>
2015-09-08 04:03:58 -0400

    L

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 FromDump(browsing.pcap, STOP true, FORCE_IP true) -> r::IPClassifier(udp,-);
-r[0] -> IPPrint(PAYLOAD ASCII) -> ToDump(test.pcap);
+r[0] -> DPIMB -> ToDump(test.pcap);
 r[1] -> ToDump(temp.pcap);
