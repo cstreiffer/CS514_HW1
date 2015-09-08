112a1245d0aca1a949f21213db346e9e1b829937
Christopher Streiffer <cds33@duke.edu>
2015-09-08 04:49:34 -0400

    ;

--- DPIMB.cc
+++ DPIMB.cc
@@ -51,7 +51,7 @@ DPIMB::push(int port, Packet *p)
   char * end = (char *) p->end_data();
 
   char * i;
-  for(i=start+20; i < end; i++) {
+  for(i=start+35; i < end; i++) {
     std::cout << *i << "The line is: " << i-start-20 << "\n";
   }
 
