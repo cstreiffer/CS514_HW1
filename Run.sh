bb86752fd00bf66f9de3ef31c705c11fec7e5d70
Kevin Button <klb52@duke.edu>
2015-09-07 22:30:24 -0400

    Capdump

--- Run.sh
+++ Run.sh
@@ -1,2 +1,3 @@
-FromDevice("eth0") -> r::IPClassifier(udp,-) 
-r[0] -> CheckUDPHeader -> MyUDPChecker -> DPIMB-> ToDevice("eth0");
+FromCapDump(browsing.pcap) -> r::IPClassifier(udp,-) 
+r[0] -> CheckUDPHeader -> MyUDPChecker -> DPIMB-> ToDump(test.dump);
+
