40fad4df8a46a3e7ca528b7939f9442ceecd8551
Christopher Streiffer <cds33@duke.edu>
2015-09-07 20:47:23 -0400

    Syntax.

--- DPIMB.hh
+++ DPIMB.hh
@@ -25,9 +25,8 @@ public:
   const char *class_name() const	{ return "DPIMB"; }
   const char *port_count() const	{ return PORTS_1_1; }
   const char *processing() const	{ return PUSH }
-  int DPIMB::initialize(ErrorHandler *errh);	
-  void DPIMB::add_handlers();
-
+  int initialize(ErrorHandler *errh);	
+  void add_handlers();
   bool check_blacklist(String url);
   Packet *simple_action(Packet *);
 
