b685c8377044813092dd9b365cc375add3db97b9
Christopher Streiffer <cds33@duke.edu>
2015-09-07 22:12:44 -0400

    :

--- Run.sh
+++ Run.sh
@@ -1,9 +1,3 @@
-#include "~/click/elements"
-
 elementclass MyUDPChecker{ input->CheckIPHeader() -> IPClassifier(udp,-) -> CheckUDPHeader -> [0]output}
-dpimb :: DPIMB {input -> output}
 
-
-FromDevice("eth0") -> MyUDPChecker;
-MyUDPChecker[0] -> dpimb;
-dpimb[0] -> ToDevice("eth0");
+FromDevice("eth0") -> MyUDPChecker -> DPIMB-> ToDevice("eth0");
