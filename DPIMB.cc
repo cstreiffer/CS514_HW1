2a3426f9b82b8d648f43355bdb9399deef7b22c9
Christopher Streiffer <cds33@duke.edu>
2015-09-08 01:44:18 -0400

    h

--- DPIMB.cc
+++ DPIMB.cc
@@ -52,7 +52,7 @@ DPIMB::push(int port, Packet *p)
   // }
 
   const click_udp * udp_port =  p->udp_header();
-  std::printf("The value of s is: %p\n", (void *) udp_port);
+  std::printf("The value of s is: %n\n", *udp_port);
 
 
   std::string url = "temp";
